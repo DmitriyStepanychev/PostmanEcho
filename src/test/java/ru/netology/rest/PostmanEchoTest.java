acb3a2d0ff33b83ce3290b4c48043e62558a6570
DmitriyStepanychev <stepanychevdmitriy@gmail.com>
2023-05-11 14:49:02 +0300

    Breaking test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -19,7 +19,7 @@ class PostmanEchoTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("data"));
     }
 
     @Test
